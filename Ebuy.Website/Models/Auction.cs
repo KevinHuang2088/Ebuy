6a8cfb2237b2e1107b737a2d67cb194b9aa101fe
Kevin Huang <KevinHuang2088@outlook.com>
2018-11-05 14:27:00 +0800

    add errormessage

--- Ebuy.Website/Models/Auction.cs
+++ Ebuy.Website/Models/Auction.cs
@@ -10,13 +10,14 @@ namespace Ebuy.Website.Models
     {
         public long Id { get;set; }
 
-        [Required,StringLength(50)]
+        [Required,StringLength(50,
+            ErrorMessage ="Title cannot be longer than 50 characters")]
         public string Title { get; set; }
 
         [Required]
         public string Description { get; set; }
 
-        [Range(1,10000)]
+        [Range(1,10000,ErrorMessage ="The auction's starting price must be at least 1")]
         public decimal StartPrice { get; set; }
         public decimal CurrentPrice { get; set; }
         public DateTime StartTime { get; set; }
